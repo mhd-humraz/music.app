fb0f11119a44fd303560742c76bc4ff250d1a6f2
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-11-08 11:38:16 +0530

    Add preloaded songs data to songs.js
    Preloaded songs data includes titles, artists, sources, and cover images.

---
+++ songs.js
@@ -0,0 +1,34 @@
+// songs.js
+// Preloaded songs data
+const songs = [
+    {
+        title: "Blinding Lights",
+        artist: "The Weeknd",
+        src: "https://assets.codepen.io/4358584/blinding-lights.mp3",
+        cover: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+    },
+    {
+        title: "Levitating",
+        artist: "Dua Lipa",
+        src: "https://assets.codepen.io/4358584/levitating.mp3",
+        cover: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+    },
+    {
+        title: "Stay",
+        artist: "The Kid LAROI & Justin Bieber",
+        src: "https://assets.codepen.io/4358584/stay.mp3",
+        cover: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+    },
+    {
+        title: "Good 4 U",
+        artist: "Olivia Rodrigo",
+        src: "https://assets.codepen.io/4358584/good-4-u.mp3",
+        cover: "https://images.unsplash.com/photo-1571330735066-03aaa9429d89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+    },
+    {
+        title: "Save Your Tears",
+        artist: "The Weeknd",
+        src: "https://assets.codepen.io/4358584/save-your-tears.mp3",
+        cover: "https://images.unsplash.com/photo-1516280440614-37939bbacd81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+    }
+];
